465cd84a18a207f6bd09e7b586e26f3938caa7a4
Jeff Juell <87201199+jefejuell@users.noreply.github.com>
2021-12-07 12:54:50 -0600

    Project updates

---
+++ map.js
@@ -0,0 +1,18 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiamVmZWp1ZWxsIiwiYSI6ImNrdGZ6aTlpaTBkOWczMm5sMDdja3UwanYifQ.0bEdPWVZftSkB8c9ZUSBOw';
+            const map = new mapboxgl.Map({
+                container: 'map',
+                style: 'mapbox://styles/mapbox/streets-v11',
+                center: [-90.370760, 38.636210],
+                zoom: 9
+            });
+
+            // Add the control to the map.
+            map.addControl(
+                new MapboxGeocoder({
+                    accessToken: mapboxgl.accessToken,
+                    mapboxgl: mapboxgl
+                })
+            );
+
+            //Adds navigation controls
+            map.addControl(new mapboxgl.NavigationControl());
